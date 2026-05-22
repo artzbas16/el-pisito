bc7f0233014f0b6498885e258b9a390996683c56
artzbas16 <aritzbasurco@gmail.com>
2026-05-22 10:55:49 +0200

    cambio de variables para que lea el backend de render

--- src/app/core/enviroments/globals.ts
+++ src/app/core/enviroments/globals.ts
@@ -1,5 +1,5 @@
-export const URL_BASE:string = 'http://localhost:8080/';
+export const URL_BASE: string = 'https://el-pisito-backend-api-rest.onrender.com/';
 export const URL_API:string = URL_BASE + 'api/';
 export const URL_AUTH:string = URL_API + 'auth/';
-export const URL_MEDIA:string = 'http://localhost:8080/';
+export const URL_MEDIA: string = 'https://el-pisito-backend-api-rest.onrender.com/';
 export const URL_IMAGEN:string = URL_BASE + 'api/imagenes/';
